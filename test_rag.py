48a55c35795fd57214240aa5e7a7d1185072960b
pgjadhav <pgjadhav0001@gmail.com>
2026-05-01 00:11:43 +0530

    Removed unnecessary files

--- test_rag.py
+++
@@ -1,8 +0,0 @@
-from app.services.rag_service import retrieve_context
-
-query = "Tell me about your experience"
-docs = retrieve_context(query)
-
-for d in docs:
-    print("\n---\n")
-    print(d.page_content)
--- test_tts.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-
-engine.say("Hello Prashant, your voice agent is working")
-engine.runAndWait()
